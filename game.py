95ad9a1a5cf5107fba84fa92fd14ee6b8580ad78
amrit <amrit.madhav.srivastava@gmail.com>
2024-07-05 16:03:50 +0000

    further refactoring

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 from ursina import *
 from game_engine.npc import NPC
 from game_engine.player import Player
+from game_engine.feature_module.npc_game import NPCGame
 
 if __name__ == '__main__':
     app = Ursina()
     player = Player(position=(10, 50, 10))
-    npc1 = NPC(player, model = 'cube', position=(0, 1, 0), collider='box', name='NPC1')    
+    npc_map = NPCGame()
+    npc1 = NPC(player, npc_map, model = 'cube', position=(0, 1, 0), collider='box', name='NPC1')    
     
     # box = Entity(model='cube', scale=(1, 1, 1), position=(0, 1, 0), collider='box')
     # text = Text(text='hi', parent=box, position=(0,1.5,0), origin=(0, 0), background=True, color=color.black, scale=10)
