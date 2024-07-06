d922fe913a142b69d39454592d366b073bfc7e83
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-07-06 15:22:48 +0530

    imported models using obj files

--- game_engine/map/map.py
+++ game_engine/map/map.py
@@ -9,6 +9,7 @@ sky = Sky()
 player = FirstPersonController()
 
 floor = Entity(model='plane', scale=(100, 0, 100), texture='grass', collider='box')
-maze = Entity(model='made',scale=10, texture='brick',position=(0,0.1,0), collider='mesh')
+maze = Entity(model='cottage', texture="brick", scale=1,position=(0,0.1,0))
+maze = Entity(model='floatingislands',scale=10,position=(0,0.1,0), collider='mesh')
 
 app.run()
