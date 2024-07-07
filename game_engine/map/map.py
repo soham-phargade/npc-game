ec35e53d8e5e9dfa1250d24cf987a54d920f4d90
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-07-07 23:13:04 +0530

    working low poly textures

--- game_engine/map/map.py
+++ game_engine/map/map.py
@@ -1,14 +1,16 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+# city_texture = load_texture('assets/obj/citybits_texture.png')
+
 app = Ursina()
 
-# EditorCamera()
+EditorCamera()
 sky = Sky()
 
-player = FirstPersonController()
+# player = FirstPersonController()
 
-floor = Entity(model='plane', scale=(100, 0, 100), texture='grass', collider='box')
-maze = Entity(model='cottage', texture="brick", scale=1,position=(0,0.1,0), double_sided=True)
+# wall = Entity(model='assets/stone_wall.fbx', texture='assets/stone_wall',scale=0.01, collider='mesh',position=(0, 0.5, 0), double_sided=True)
+city = Entity(model='assets/obj/box_B', texture = 'assets/obj/citybits_texture.png')
 
 app.run()
