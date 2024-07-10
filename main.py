9ca4279feb8f820a72c29843cb74f341b3f38e57
soham-phargade <soham.g.phargade@gmail.com>
2024-07-10 12:23:54 +0530

    deleted t2.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from game_engine.feature_module import gemini_api
-from t2 import Game
+#from t2 import Game
+from game_engine.feature_module.gemini_api import gemini
+
+class Game:
+    def __init__(self):
+        self.convo = []
+        self.npcs = 0
+
+    def welcome_message(self):
+        print("Welcome to multi AI convo")
+        print("How many Robots do you want to talk to?")
+    
+    def response(self, id, convo_history):
+        prompt = f"You are Robot {id}. Based on the provided chat history, respond briefly."
+        response = gemini(prompt, convo_history)
+        return response
 
 def main():
     game = Game()
